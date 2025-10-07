799f0f76d6dbbe2c99c73e1c62451745b1d44a50
Renan Girotto <me.renangirotto+dev@gmail.com>
2025-10-07 01:44:45 -0300

    feat: add NavbarMobile component

---
+++ app/components/NavbarMobile.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav>
+    <button class="button-menu" aria-label="Abrir menu">
+      <Icon name="menu" />
+    </button>
+  </nav>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped>
+.button-menu {
+  --icon-width: 24px;
+  --icon-height: 24px;
+
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-block: 8px;
+  padding-inline: 8px;
+  border: none;
+  border-radius: 4px;
+  background: none;
+}
+</style>

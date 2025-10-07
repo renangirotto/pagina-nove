878a8eba6043d4ec739b047f22868e1e03bef5a1
Renan Girotto <me.renangirotto+dev@gmail.com>
2025-10-07 01:44:53 -0300

    feat: add default layout

---
+++ app/layouts/default.vue
@@ -0,0 +1,38 @@
+<template>
+  <header class="header">
+    <Container class="header-container">
+      <!-- <figure>
+      <img src="/logo.png" alt="Logo" width="150" height="150" />
+    </figure> -->
+
+      <NavbarMobile />
+    </Container>
+  </header>
+  <main>
+    <slot />
+  </main>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped>
+.header {
+  padding-inline: 24px;
+
+  &:not(:last-child) {
+    margin-block-end: 16px;
+
+    .header-container {
+      border-block-end: 1px solid var(--color-grey-light-100);
+    }
+  }
+}
+
+.header-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  padding-block: 16px;
+}
+</style>

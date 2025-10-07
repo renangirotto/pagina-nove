7a0d4296f01860f9b465f307526af27330c6558a
Renan Girotto <me.renangirotto+dev@gmail.com>
2025-10-07 01:43:56 -0300

    feat: add global css import and NuxtLayout on app

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+import "~/assets/css/reset.css";
+import "~/assets/css/colors.css";
+import "~/assets/css/typography.css";
+</script>
+
+<style></style>

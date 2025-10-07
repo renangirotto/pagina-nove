6218108d010f9ca2d23aed11ce8510b727de7d0b
Renan Girotto <me.renangirotto+dev@gmail.com>
2025-10-07 01:44:09 -0300

    feat: add Icon component

---
+++ app/components/Icon.vue
@@ -0,0 +1,15 @@
+<template>
+  <nuxt-icon :name="name" filled />
+</template>
+
+<script lang="ts" setup>
+defineProps<{ name: string }>();
+</script>
+
+<style>
+.nuxt-icon svg {
+  width: var(--icon-width, 24px);
+  height: var(--icon-height, 24px);
+  margin-bottom: 0;
+}
+</style>

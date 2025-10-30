c395db1f5a8a15cc6e2f24aa4448ecb9936a93c6
Renan Girotto <me.renangirotto+dev@gmail.com>
2025-10-30 00:40:51 -0300

    chore: add global configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt(
-  // Your custom configs here
+export default withNuxt({
   rules: {
-      'vue/no-multiple-template-root': 'off'
-  }
-)
+    "vue/html-self-closing": "off",
+  },
+});
+// Your custom configs here

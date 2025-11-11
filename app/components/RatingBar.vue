8cded15c74fde67b049a7978282d5096d814a8e9
Renan Girotto <me.renangirotto+dev@gmail.com>
2025-11-11 01:20:32 -0300

    feat: add RatingBar component

---
+++ app/components/RatingBar.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="icons">
+    <template v-for="n in filledStars" :key="'filled-' + n">
+      <custom-icon name="star"></custom-icon>
+    </template>
+
+    <custom-icon
+      v-if="hasHalfStar"
+      custom-class="icon-half"
+      name="half-star"
+    ></custom-icon>
+
+    <template v-for="n in emptyStars" :key="'empty-' + n">
+      <custom-icon custom-class="icon-empty" name="star"></custom-icon>
+    </template>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed } from "vue";
+
+const MAX_RATING = 5;
+
+const props = defineProps<{ rating: string | number }>();
+
+const ratingAsNumber = computed(() => Number(props.rating));
+
+const filledStars = computed(() => {
+  return Math.floor(ratingAsNumber.value);
+});
+
+const hasHalfStar = computed(() => {
+  const decimalPart = ratingAsNumber.value - filledStars.value;
+  return decimalPart >= 0.5 - 1e-6;
+});
+
+const emptyStars = computed(() => {
+  let count = MAX_RATING;
+  count -= filledStars.value;
+
+  if (hasHalfStar.value) {
+    count -= 1;
+  }
+
+  return count;
+});
+</script>
+
+<style>
+.icons {
+  --icon-size: 24px;
+  display: flex;
+  align-items: center;
+  gap: 4px;
+}
+
+.icon-empty {
+  --icon-color-light: #e7e7ef;
+}
+
+.icon-half {
+  --icon-color-dark: var(--color-purple-100);
+  --icon-color-light: #e7e7ef;
+}
+</style>

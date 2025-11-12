617dadfb1dd64d23c0d92896248d13b7e04f67ab
Renan Girotto <me.renangirotto+dev@gmail.com>
2025-11-12 00:30:14 -0300

    feat: improved default layout styles and layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,14 +1,6 @@
 <template>
   <div>
-    <!-- <header class="header">
-      <LayoutContainer class="header-container">
-        <figure>
-      <img src="/logo.png" alt="Logo" width="150" height="150" />
-    </figure>
-
-        <NavbarMobile />
-      </LayoutContainer>
-    </header> -->
+    <header-navbar />
     <main class="layout-main">
       <slot />
     </main>
@@ -18,28 +10,12 @@
 <script lang="ts" setup></script>
 
 <style lang="scss" scoped>
-.header {
-  padding-inline: 24px;
-
-  &:not(:last-child) {
-    margin-block-end: 16px;
-
-    .header-container {
-      border-block-end: 1px solid var(--color-grey-light-100);
-    }
-  }
-}
-
-.header-container {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  padding-block: 16px;
-}
-
 .layout-main {
   padding-block: 32px;
   padding-inline: 16px;
+
+  @include container-desktop {
+    padding-block: 40px;
+  }
 }
 </style>

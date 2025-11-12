ca5e7bebecb4e0e39c6043bb4577369bfc8a4e33
Renan Girotto <me.renangirotto+dev@gmail.com>
2025-11-12 00:27:51 -0300

    feat: add routes and nav items constants

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -1,1 +1,26 @@
-export const PATH_IMG_COVERS = '/images/covers'
+export const PATH_IMG_COVERS = '/images/covers';
+
+export const ROUTES = {
+  buy: '/onde-comprar',
+  home: '/',
+  notes: '/notas',
+  readings: '/leituras'
+};
+
+export const NAV_ITEMS = [
+  {
+    icon: 'pen-tool',
+    label: 'notas de leitura',
+    to: ROUTES.notes
+  },
+  {
+    icon: 'triangle-flag',
+    label: 'leituras finalizadas',
+    to: ROUTES.readings
+  },
+  {
+    icon: 'shopping-bag',
+    label: 'onde comprar',
+    to: ROUTES.buy
+  }
+]

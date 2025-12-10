3e5ecee1d62b8876af7bc0fba6780ac1ed3e2e00
Renan Girotto <me.renangirotto+dev@gmail.com>
2025-12-10 12:52:45 -0300

    feat: add new content schemas

--- content.config.ts
+++ content.config.ts
@@ -1,24 +1,35 @@
-import { defineCollection, defineContentConfig } from '@nuxt/content'
-import { z } from 'zod'
+import { defineCollection, defineContentConfig } from "@nuxt/content";
+import { z } from "zod";
 
 export default defineContentConfig({
   collections: {
     leituras: defineCollection({
-      type: 'page',
-      source: 'leituras/*.md',
+      type: "page",
+      source: "leituras/*.md",
       schema: z.object({
         artists: z.array(z.string()),
         collection: z.string().optional(),
         collectionVolume: z.number().optional(),
         cover: z.string(),
         date: z.date(),
+        dateNote: z.date().optional(),
         labels: z.array(z.string()),
         pages: z.string(),
         publisher: z.string(),
         publishYear: z.string(),
         rating: z.string(),
         title: z.string(),
-      })
-    })
-  }
-})
+      }),
+    }),
+    notas: defineCollection({
+      type: "page",
+      source: "notas/*.md",
+      schema: z.object({
+        collection: z.string().optional(),
+        cover: z.string(),
+        dateNote: z.date(),
+        title: z.string(),
+      }),
+    }),
+  },
+});

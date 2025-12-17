66e640b149ff3adbfd55061174e2b30be90a3db9
Renan Girotto <me.renangirotto+dev@gmail.com>
2025-12-17 19:19:00 -0300

    feat: add comic store initial content

---
+++ app/components/ComicStore.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="store">
+    <figure aria-hidden="true">
+      <nuxt-img class="store-image" :src="imageUrl" alt="" />
+    </figure>
+    <div class="store-info">
+      <h3 class="store-title">{{ title }}</h3>
+      <p class="store-links">
+        <a class="store-link" :href="instagram.link" target="_blank">
+          {{ instagram.text }}
+        </a>
+        <custom-icon name="medical-cross-symbol" />
+        <a class="store-link" :href="siteUrl" target="_blank">
+          {{ siteUrl }}
+        </a>
+      </p>
+      <a class="store-link" :href="address.link" target="_blank">
+        {{ address.text }}
+      </a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  address: { link: string; text: string };
+  imageUrl: string;
+  instagram: { link: string; text: string };
+  siteUrl: string;
+  title: string;
+}>();
+</script>
+
+<style lang="scss" scoped>
+.store {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: auto auto;
+
+  @include container-desktop {
+    grid-template-columns: 400px 1fr;
+    grid-template-rows: auto;
+  }
+
+  &:not(:last-child) {
+    margin-block-end: 40px;
+  }
+}
+
+.store-links {
+  --color-store-link: var(--color-grey-light-200);
+  --icon-size: 12px;
+  --icon-color-light: var(--color-grey-light-200);
+  display: flex;
+  align-items: center;
+  gap: 12px;
+
+  &:not(:last-child) {
+    margin-block-end: 24px;
+  }
+}
+
+.store-link {
+  font-size: clamp(1rem, 3vw, 1.125rem);
+  font-weight: 600;
+  color: var(--color-store-link);
+  text-decoration: none;
+
+  &:hover,
+  &:focus-within {
+    text-decoration: underline;
+  }
+}
+
+.store-title {
+  font-size: clamp(1.5rem, 3vw, 2rem);
+
+  &:not(:last-child) {
+    margin-block-end: 4px;
+  }
+}
+</style>
--- app/pages/onde-comprar/index.vue
+++ app/pages/onde-comprar/index.vue
@@ -1,11 +1,79 @@
 <template>
-  <layout-container>
-    <title-page>
-      <custom-icon aria-hidden="true" name="shopping-bag" /> Onde Comprar
-    </title-page>
-  </layout-container>
+  <section>
+    <layout-container>
+      <title-page>
+        <custom-icon aria-hidden="true" name="shopping-bag" /> Onde Comprar
+      </title-page>
+      <p class="page-text">
+        Se você está por <strong>São Paulo</strong> e quer apoiar lojas de
+        quadrinhos da região? Aqui fica a recomendação de onde comprar!
+      </p>
+
+      <div>
+        <comic-store
+          v-for="store in stores"
+          :key="store.title"
+          :address="store.address"
+          :image-url="`/lojas/${store.imageUrl}`"
+          :instagram="store.instagram"
+          :site-url="store.siteUrl"
+          :title="store.title"
+        />
+      </div>
+    </layout-container>
+  </section>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const stores = [
+  {
+    address: {
+      link: "https://maps.app.goo.gl/gmN6YRBroyq5jHy56",
+      text: "Praça Benedito Calixto, 158 - 1º andar - Pinheiros, São Paulo - SP, 05406-040",
+    },
+    imageUrl: "monstra.webp",
+    instagram: {
+      link: "https://www.instagram.com/lojamonstra/",
+      text: "@lojamonstra",
+    },
+    siteUrl: "https://lojamonstra.com.br/",
+    title: "Loja Monstra",
+  },
+  {
+    address: {
+      link: "https://maps.app.goo.gl/4VKBfjpaYxg85tZYA",
+      text: "R. Coelho Lisboa, 366 - Tatuapé, São Paulo - SP, 03323-040",
+    },
+    imageUrl: "comicboom.webp",
+    instagram: {
+      link: "https://www.instagram.com/comic_boom_/",
+      text: "@comic_boom_",
+    },
+    siteUrl: "https://comicboom.com.br/",
+    title: "Comic boom!",
+  },
+  {
+    address: {
+      link: "https://maps.app.goo.gl/pvoqRDo4EtXzPSkx7",
+      text: "R. da Consolação, 2685 - Cerqueira César, São Paulo - SP, 01416-001",
+    },
+    imageUrl: "comix.webp",
+    instagram: {
+      link: "https://www.instagram.com/comixbookshop/",
+      text: "@comixbookshop",
+    },
+    siteUrl: "https://www.comix.com.br/",
+    title: "Comix Book Shop",
+  },
+];
+</script>
 
-<style></style>
+<style lang="scss" scoped>
+.page-text {
+  font-size: clamp(1rem, 3vw, 1.125rem);
+
+  &:not(:last-child) {
+    margin-block-end: 72px;
+  }
+}
+</style>

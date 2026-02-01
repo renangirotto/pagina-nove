098b7de966571be75aadd31f980b504dfaa37643
Renan Girotto <me.renangirotto+dev@gmail.com>
2026-02-01 20:34:52 -0300

    chore: config for new custom domain github pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
+    baseURL: "/",
     head: {
       title: "Página Nove | Leituras e textos mal escritos",
       htmlAttrs: {
